997c5aaeb61a7e4c3beaf8ebf895657b27a6dddc
chirag gupta <chiragg6@gmail.com>
2021-05-18 19:59:43 +0530

    channels flow added

--- waitgroup/main.go
+++ waitgroup/main.go
@@ -8,11 +8,14 @@ import (
 func main() {
 
 	var wg sync.WaitGroup
+	ch := make(chan string)
 	wg.Add(1)
-	go func() {
+	go func(ch chan string) {
 		fmt.Println("Running go routine")
 		defer wg.Done()
-	}()
-
+		ch <- "testing channels"
+	}(ch)
+	a := <-ch
+	fmt.Println("Printing value from channel", a)
 	wg.Wait()
 }
